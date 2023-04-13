e10a1284521c64ab34ae78d7230bc67b3d019d2d
ReyGrau <indrei457@gmail.com>
2023-04-13 12:47:14 +0300

    Create val-stats command

---
+++ commands/val-stats.js
@@ -0,0 +1,46 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('val_stats')
+        .setDescription('Your valorant statistics')
+        .addStringOption((option) =>
+            option
+                .setName('name')
+                .setDescription('You account name, like "xShodi"')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('tag')
+                .setDescription('You account tag, like "2312"')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('region')
+                .setDescription('You account region, like "eu"')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        try {
+            const name = interaction.options.getString('name');
+            const tag = interaction.options.getString('tag');
+            const region = interaction.options
+                .getString('region')
+                ?.toLowerCase();
+            await interaction.reply(`Please, wait ღゝ◡╹ )ノ♡`);
+            const res = await axios.get(
+                `https://api.henrikdev.xyz/valorant/v3/matches/${region}/${name}/${tag}`
+            );
+            console.log(res);
+            // if (!res?.data?.results) {
+            //     await interaction.reply('Something went wrong (ノ﹏ヽ)');
+            // }
+        } catch (err) {
+            console.log(err);
+        }
+    },
+};
