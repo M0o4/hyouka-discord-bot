2af44520524658e6e47d981df009f5e2fcd5520a
ReyGrau <indrei457@gmail.com>
2023-04-13 11:02:21 +0300

    Fix guild commands and add skip command

--- commands/skip.js
+++ commands/skip.js
@@ -2,8 +2,8 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('stop')
-        .setDescription('Stops player'),
+        .setName('skip')
+        .setDescription('Stops song'),
     async execute(interaction) {
         const guildId = interaction.guild.id;
         const guildQueue = interaction.client.player.getQueue(guildId);
