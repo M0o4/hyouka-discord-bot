96e119918b01fc6ba4f7b5a48344213f28dbd00f
ReyGrau <indrei457@gmail.com>
2023-04-13 11:06:22 +0300

    Fix skip text

--- commands/skip.js
+++ commands/skip.js
@@ -7,12 +7,13 @@ module.exports = {
     async execute(interaction) {
         const guildId = interaction.guild.id;
         const guildQueue = interaction.client.player.getQueue(guildId);
+        console.log(guildQueue);
         if (guildQueue) {
             guildQueue.skip();
-            await interaction.reply('Stopping player! (✪‿✪)ノ');
+            await interaction.reply(`Skipping song! (✪‿✪)ノ`);
             return;
         }
 
-        await interaction.reply('There is no players to stop!');
+        await interaction.reply(`There is no song to skip ┐ ( -“-) ┌`);
     },
 };
