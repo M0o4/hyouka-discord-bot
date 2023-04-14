d0b93697e369c4423f874286d30ebf76e550c20b
Vladislav Indrey <v.indrej@nv.dunice.net>
2023-04-14 20:14:39 +0300

    Create anime-rand command

---
+++ commands/anime-rand.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { createMalEmbed } = require('../utils');
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('anime_rand')
+        .setDescription('Get random anime'),
+    async execute(interaction) {
+        try {
+            await interaction.reply(`Please, wait ღゝ◡╹ )ノ♡`);
+            const res = await axios.get(
+                `https://api.jikan.moe/v4/random/anime`
+            );
+            const anime = res?.data?.data;
+            if (!anime) {
+                await interaction.followUp('Something went wrong (ノ﹏ヽ)');
+                return;
+            }
+
+            await interaction.followUp({ embeds: [createMalEmbed(anime)] });
+        } catch (err) {
+            await interaction.followUp('Something went wrong (ノ﹏ヽ)');
+            console.log(err);
+        }
+    },
+};
