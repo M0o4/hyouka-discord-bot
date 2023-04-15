0402e99a3abdbf47bed5883ab76d77641435216c
Vladislav Indrey <v.indrej@nv.dunice.net>
2023-04-15 23:45:43 +0300

    Fix error with ytsr and print guild and time in logs

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,9 +1,13 @@
 const { Events } = require('discord.js');
+const moment = require('moment');
 
 module.exports = {
     name: Events.InteractionCreate,
     async execute(interaction) {
         if (!interaction.isChatInputCommand()) return;
+
+        const date = new Date();
+        const formattedDate = moment(date).format('MMMM Do, HH:mm');
 
         const command = interaction.client.commands.get(
             interaction.commandName
@@ -11,17 +15,21 @@ module.exports = {
 
         if (!command) {
             console.error(
-                `No command matching ${interaction.commandName} was found.`
+                `No command matching ${interaction.commandName} was found | ${interaction.guild.name} | ${formattedDate}`
             );
             return;
         }
 
-        console.log(`Try execute command ${interaction.commandName}`);
+        console.log(
+            `Try execute command ${interaction.commandName} | ${interaction.guild.name} | ${formattedDate} `
+        );
 
         try {
             await command.execute(interaction);
         } catch (error) {
-            console.error(`Error executing ${interaction.commandName}`);
+            console.error(
+                `Error executing ${interaction.commandName} | ${interaction.guild.name} | ${formattedDate}`
+            );
             console.error(error);
         }
     },
